9dd43acee588e45b53e7e9aff0cc09aaa6dd4cc0
Aren Hovsepyan <ar.hovsepyan2011@gmail.com>
2024-02-25 04:22:33 +0400

    feat: add revalidation API endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+export const dynamic = "force-static";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Next AWS Fargate
+      <div>
+        <div>Next AWS Fargate</div>
+        <div>Generated at: {new Date().toISOString()}</div>
+      </div>
     </main>
   );
 }
